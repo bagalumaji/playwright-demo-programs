3f2667902c5b55282b923f15d4310456b53c02ea
bagalumaji <bagalumaji@gmail.com>
2023-11-03 11:31:56 +0530

    skip test

--- tests/first.spec.js
+++ tests/first.spec.js
@@ -3,3 +3,7 @@ const { test, expect } = require("@playwright/test");
 test("my demo test", async function ({ page }) {
   console.log("my demo test");
 });
+
+test.skip("my demo skip test", async function ({ page }) {
+  console.log("my demo skip test");
+});
