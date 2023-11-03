2e0de9b778a81b57a69d87a8c8c0c2c45f8202a8
bagalumaji <bagalumaji@gmail.com>
2023-11-03 11:35:42 +0530

    test with describe block

---
+++ tests/second.spec.js
@@ -0,0 +1,6 @@
+const { test, expect } = require("@playwright/test");
+test.describe("login demo test", function () {
+  test("logiin 1", async function ({ page }) {
+    console.log("demo login test");
+  });
+});
